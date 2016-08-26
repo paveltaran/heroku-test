c2586c4b6a73de5895830940d77f3edc03551cd9
Pavel Taran <pavel.taran@gmail.com>
2016-08-26 17:52:45 +0300

    1

--- checker.js
+++ checker.js
@@ -1,9 +1,9 @@
-/*require('babel-register')({
+require('babel-register')({
 	"presets": ["es2015", "react", "stage-0", "stage-1"],
 	"plugins": ["transform-decorators-legacy"],
 	ignore: false,
-	only: /(db)|(checker)/
-});*/
+	only: /(db)|(checker\.js)/
+});
 const db = require('db');
 const debug = require('debug')(__filename);
 import autobind from 'autobind-decorator'
