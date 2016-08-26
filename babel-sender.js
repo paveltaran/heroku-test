c74ad23ee2c283f1dc46a8088c15a5c4c86a5c8f
Pavel Taran <pavel.taran@gmail.com>
2016-08-26 22:17:20 +0300

    1

---
+++ babel-sender.js
@@ -0,0 +1,9 @@
+process.env.RETHINKDB_URL = "rethinkdb://46.8.45.97/gambling";
+process.env.RABBITMQ_URL = "amqp://yourName:yourPass123@46.8.45.97";
+require('babel-register')({
+	"presets": ["es2015", "react", "stage-0", "stage-1"],
+	"plugins": ["transform-decorators-legacy"],
+	ignore: false,
+	only: /(db)|(checker\.js)|(sender\.js)/
+});
+require(__dirname+'/sender.js');
