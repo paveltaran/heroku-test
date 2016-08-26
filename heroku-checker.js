1347e7a878d91f58c2a8d3e8777beccbc9144445
Pavel Taran <pavel.taran@gmail.com>
2016-08-26 18:54:30 +0300

    1

--- heroku-checker.js
+++ heroku-checker.js
@@ -1,6 +1,6 @@
 require('babel-register')({
 	"presets": ["es2015", "react", "stage-0", "stage-1"],
-	"plugins": ["transform-decorators"],
+	"plugins": ["transform-decorators-legacy"],
 	ignore: false,
 	only: /(db)|(checker\.js)/
 });
